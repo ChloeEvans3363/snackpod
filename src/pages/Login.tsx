6a62383389f6df400518d99c3cddd0ccc8f29723
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2026-01-13 11:58:36 -0500

    Revert login test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,43 @@
-import { FileUploader } from "@aws-amplify/ui-react-storage";
-import "@aws-amplify/ui-react/styles.css";
+import Button from "../components/Button";
+import TextInput from "../components/Form";
+import logo from "../assets/logo_main.svg";
+import "../style/Login.css";
+import { Link } from "react-router-dom";
 
-export const DefaultFileUploaderExample = () => {
+export function Login() {
   return (
-    <FileUploader
-      acceptedFileTypes={["image/*"]}
-      path="public/"
-      maxFileCount={1}
-      isResumable
-    />
+    <>
+      <div className="loginLogo">
+        <Link to="/">
+          <img src={logo} className="logo snackpod" alt="Snackpod logo" />
+        </Link>
+        <h2>snackable audio, seriously addictive</h2>
+      </div>
+
+      <div className="inputContainer">
+        <TextInput type="text" value="email">
+          Email
+        </TextInput>
+        <TextInput type="password" value="password">
+          Password
+        </TextInput>
+      </div>
+
+      <div className="buttonContainer">
+        <Link to="/Feed">
+          <Button color="primary" buttonName="loginPage">
+            Log In
+          </Button>
+        </Link>
+        <p>OR</p>
+        <Link to="/Feed">
+          <Button color="outline-secondary" buttonName="loginPage">
+            Sign Up
+          </Button>
+        </Link>
+      </div>
+    </>
   );
-};
-export default DefaultFileUploaderExample;
+}
+
+export default Login;
