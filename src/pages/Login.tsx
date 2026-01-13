6b224c271fa0df1f222a837410877cb805c28c16
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2026-01-13 11:53:21 -0500

    Upload test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,14 @@
-import Button from "../components/Button";
-import TextInput from "../components/Form";
-import logo from "../assets/logo_main.svg";
-import "../style/Login.css";
-import { Link } from "react-router-dom";
+import { FileUploader } from "@aws-amplify/ui-react-storage";
+import "@aws-amplify/ui-react/styles.css";
 
-export function Login() {
+export const DefaultFileUploaderExample = () => {
   return (
-    <>
-      <div className="loginLogo">
-        <Link to="/">
-          <img src={logo} className="logo snackpod" alt="Snackpod logo" />
-        </Link>
-        <h2>snackable audio, seriously addictive</h2>
-      </div>
-
-      <div className="inputContainer">
-        <TextInput type="text" value="email">
-          Email
-        </TextInput>
-        <TextInput type="password" value="password">
-          Password
-        </TextInput>
-      </div>
-
-      <div className="buttonContainer">
-        <Link to="/Feed">
-          <Button color="primary" buttonName="loginPage">
-            Log In
-          </Button>
-        </Link>
-        <p>OR</p>
-        <Link to="/Feed">
-          <Button color="outline-secondary" buttonName="loginPage">
-            Sign Up
-          </Button>
-        </Link>
-      </div>
-    </>
+    <FileUploader
+      acceptedFileTypes={["image/*"]}
+      path="public/"
+      maxFileCount={1}
+      isResumable
+    />
   );
-}
-
-export default Login;
+};
+export default DefaultFileUploaderExample;
