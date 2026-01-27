b50d7fd2103fe3a98a34f61f6b7e0e708a7047b0
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2026-01-27 17:30:31 -0500

    Temporary test on new upload system

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,30 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+// const schema = a.schema({
+//   Todo: a.model({
+//     content: a.string(),
+//   }).authorization(allow => [allow.owner()]),
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     // This tells the data client in your app (generateClient())
+//     // to sign API requests with the user authentication token.
+//     defaultAuthorizationMode: 'userPool',
+//   },
+// });
+
 const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization(allow => [allow.owner()]),
+  Podcast: a
+    .model({
+      id: a.id().required(),
+      name: a.string().required(),
+      coverArtPath: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -11,8 +32,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    // This tells the data client in your app (generateClient())
-    // to sign API requests with the user authentication token.
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: "apiKey",
+
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
