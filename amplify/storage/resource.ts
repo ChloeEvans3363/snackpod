348a940fbb76aad274862b2c209fd7f6d32b7ace
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2026-01-27 18:52:32 -0500

    Changed authentication

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: "amplify-gen2-files",
   access: (allow) => ({
-    "podcast-submissions/*": [allow.authenticated.to(["read", "write", "delete"])],
+    "podcast-submissions/*": [allow.guest.to(["read", "write", "delete"])],
   }),
 });
 
